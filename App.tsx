671e7f87240d0e992551d50cd57a6ca206dcd2b2
varathsurya <m.varathsurya@gmail.com>
2023-12-02 01:17:06 +0530

    Amatic font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,32 @@
+import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, ActivityIndicator } from "react-native";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
+import { AmaticSC_700Bold, AmaticSC_400Regular } from "@expo-google-fonts/amatic-sc";
+import * as SplashScreen from "expo-splash-screen";
+
 import DayListItem from "./src/components/core/DayListItem";
+
+SplashScreen.preventAutoHideAsync();
 
 const days = [...Array(24)].map((val, index) => index + 1);
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    Inter: Inter_900Black,
+    Amatic: AmaticSC_400Regular,
+    AmaticBold: AmaticSC_700Bold
+  });
+
+  useEffect(() => {
+    if (fontsLoaded || fontError) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
   return (
     <View style={styles.container}>
       <FlatList
--- src/components/core/DayListItem.tsx
+++ src/components/core/DayListItem.tsx
@@ -29,5 +29,6 @@ const styles = StyleSheet.create({
   text: {
     color: "#9b4521",
     fontSize: 70,
+    fontFamily:'AmaticBold'
   },
 });
